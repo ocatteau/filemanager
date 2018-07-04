9e53306650c7385d758f82e897cef9ee168fbdca
Olivier Catteau <ocatteau@gmail.com>
2018-07-04 21:33:16 +0200

    Cleaning + logger

--- src/main/java/fr/ocatteau/resources/EncryptionManager.java
+++ src/main/java/fr/ocatteau/resources/EncryptionManager.java
@@ -1,5 +1,8 @@
 package fr.ocatteau.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -13,9 +16,17 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Encryption / decryption component.
+ */
 public class EncryptionManager {
+	private static final Logger LOG = LoggerFactory.getLogger(EncryptionManager.class);
 	private String key;
 
+	/**
+	 * Constructor.
+	 * @param key the encryption key
+	 */
 	public EncryptionManager(String key) {
 		this.key = key;
 	}
@@ -27,7 +38,12 @@ public class EncryptionManager {
 	 * @throws IOException
 	 */
 	public void encrypt(File inputFile, File encryptedFile) throws IOException {
-		fileProcessor(Cipher.ENCRYPT_MODE, key, inputFile, encryptedFile);
+		try {
+			fileProcessor(Cipher.ENCRYPT_MODE, key, inputFile, encryptedFile);
+		} catch (IOException e) {
+			LOG.error("Can't encrypt file : %s", encryptedFile.getName());
+			throw e;
+		}
 	}
 
 	/**
@@ -37,35 +53,31 @@ public class EncryptionManager {
 	 * @throws IOException
 	 */
 	public void decrypt(File encryptedFile, File decryptedFile) throws IOException {
-		fileProcessor(Cipher.DECRYPT_MODE, key, encryptedFile, decryptedFile);
+		try {
+			fileProcessor(Cipher.DECRYPT_MODE, key, encryptedFile, decryptedFile);
+		} catch (IOException e) {
+			LOG.error("Can't decrypt file : %s", encryptedFile.getName());
+			throw e;
+		}
 	}
 
 	private void fileProcessor(int cipherMode, String key, File inputFile, File outputFile) throws IOException {
-		FileInputStream inputStream = null;
-		FileOutputStream outputStream = null;
-		try {
+		try(FileInputStream inputStream = new FileInputStream(inputFile);
+			FileOutputStream outputStream = new FileOutputStream(outputFile)) {
 			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
 			Cipher cipher = Cipher.getInstance("AES");
 			cipher.init(cipherMode, secretKey);
 
-			inputStream = new FileInputStream(inputFile);
 			byte[] inputBytes = new byte[(int) inputFile.length()];
 			inputStream.read(inputBytes);
 
 			byte[] outputBytes = cipher.doFinal(inputBytes);
 
-			outputStream = new FileOutputStream(outputFile);
+
 			outputStream.write(outputBytes);
 		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
 				| IllegalBlockSizeException | IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (inputStream != null) {
-				inputStream.close();
-			}
-			if (outputStream != null) {
-				outputStream.close();
-			}
+			throw new IOException(String.format("Can't process file : %s", inputFile.getName()), e);
 		}
 	}
 }
